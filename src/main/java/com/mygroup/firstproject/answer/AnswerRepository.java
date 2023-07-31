62993485d563e9f9af713113f46709229d599807
rhkr8521 <rhkr8521@gmail.com>
2023-07-31 10:35:11 +0900

    first commit

---
+++ src/main/java/com/mygroup/firstproject/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mygroup.firstproject.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
