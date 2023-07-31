96cb64b62987ba45f7bfa50f72287fc106609335
rhkr8521 <rhkr8521@gmail.com>
2023-07-31 15:50:12 +0900

    add user entity on question and answer

--- src/main/java/com/mygroup/firstproject/user/UserService.java
+++ src/main/java/com/mygroup/firstproject/user/UserService.java
@@ -1,7 +1,10 @@
 package com.mygroup.firstproject.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import com.mygroup.firstproject.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,14 +13,23 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
+    }
+
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+        if (siteUser.isPresent()) {
+            return siteUser.get();
+        } else {
+            throw new DataNotFoundException("siteuser not found");
+        }
     }
 }
